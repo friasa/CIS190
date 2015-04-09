1c636a70429b6560017e2f5265db880786d952f4
Alex Frias <frias@seas.upenn.edu>
2015-04-09 12:16:29 -0400

    DE Alg.

---
+++ de_alg.cpp
@@ -0,0 +1,11 @@
+/* Deferred Acceptance Algorithm
+ * Authors: Alex Frias and Alex Peckman
+ * Description: The deferred acceptance algorithm is a matching 
+ *     algorithm that takes in a two groups, x and y, and a list of 
+ *     preferences from x over the other group, y. It then 
+ *     computes the socially optimal stable matching for x. 
+ * Dependencies:
+ *     A list x 
+ *     A list y
+ *     A set of preferences of each x onto y
+ */
