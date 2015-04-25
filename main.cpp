0af1a0ef2115f1ef0b543a27a81efe47c41feb65
Alex Peckman <alexpeckman@Alexs-MacBook-Pro.local>
2015-04-25 19:46:27 -0400

    sup sexy bitches

--- main.cpp
+++ main.cpp
@@ -24,42 +24,191 @@ void printStudents(list<Student*> students){
 
 int main(int argc, const char* argv[]){
 
-  char c = "f";
-  string str = "";
-  str.append(c);
-  cout << c;
-
   // check usage
   if (argc != 3){
     cout << "Usage: ./main [student file] [course file]\n"; 
     exit(-1);
   }
 
+
+
+  list<Course*> course_register;
+  list<Student*> all_students;
+  
+
   // parse input
   string s_file_name, c_file_name;
+  ifstream s_file, c_file;
   s_file_name = argv[1];
   c_file_name = argv[2];
 
-  // hello
-  // parse files
-  list<Student*> students;
-  list<Course*> courses;
-  ifstream s_file, c_file;
-  s_file.open(s_file_name);
-  string student_name = "";
-  while (s_file.good()){
-    // read in the file and make students
+  // parse c_file
+  c_file.open(c_file_name);
+  int capacity;
+  int i;
+  //char cap_str[10] = "";
+  string cap_str;
+  char* c = new char [1];
+  string course_name;
+  Course* temp_course;
+  //while (c_file.good()){
+  while (!c_file.eof()) {
+    // read in the file and make course
+
+    course_name = "";
+
+    // get course name
+    while(true) {
+      c_file.read(c, 1);
+      if (strcmp(c, ":") == 0) {
+	break;
+      }
+      course_name.append(c);
+    }
+
+    // get capacity
+    i = 0;
+    cap_str = "";
+    c_file.read(c, 1); // skip over the space
+    while (true) {
+      c_file.read(c, 1);
+      if (strcmp(c, ";") == 0) {
+	break;
+      }
+      cap_str.append(c);
+      i++;
+    }
+    capacity = stoi(cap_str);
+
+    
+    temp_course = new Course(course_name, capacity);
+    course_register.push_front(temp_course);
+
+    // read chars until get to the beginning of next course
+    while (true) {
+      c_file.read(c, 1);
+      if (strcmp(c, "\n") == 0) {
+	break;
+      }
+      else if (strcmp(c, "#") == 0) {
+	goto C_FILE_DONE;
+      }
+    }
     
   }
-  s_file.close();
-  c_file.open(c_file_name);
-  while (c_file.good()){
-    // read in the file and make course
-  }
+
+ C_FILE_DONE:
   c_file.close();
 
+  
+  // parse s_file
+  list<Course*> course_prefs;
+  list<string> course_names;
+  s_file.open(s_file_name);
+  string student_name = "";
+  string course = "";
+  Student* temp;
+  float gpa;
+  i = 0;
+  //char gpa_str[10] = "";
+  string gpa_str;
+  while (s_file.good()) {
+    // read in the file and make students
+    //Student* stud = new Student();
+
+    student_name = "";
+    course_names.clear();
+    course_prefs.clear();
+
+    
+    // get student name
+    while(true) {
+      s_file.read(c, 1);
+      if (strcmp(c, ":") == 0) {
+	break;
+      }
+      student_name.append(c);
+    }
+
+    // get gpa
+    i = 0;
+    gpa_str = "";
+    s_file.read(c, 1); // read the space
+    while (true) {
+      s_file.read(c, 1);
+      if (strcmp(c, ",") == 0) {
+	break;
+      }
+      gpa_str.append(c);
+      i++;
+    }
+    gpa = stof(gpa_str);
+
+
+    // get student's preferences
+    while (true) {
+      s_file.read(c, 1);
+      if (strcmp(c, "{") == 0) {
+        break;
+      }
+    }
+    
+    while (true) {
+      s_file.read(c, 1);
+      if (strcmp(c, "}") == 0) {
+	course_names.push_front(course);
+	course = "";
+        break;
+      }
+      if (strcmp(c, " ") == 0) {
+	course_names.push_front(course);
+	course = "";
+	continue;
+      }
+      course.append(c);
+    }
+    
+    
+    // make a list of courses from the list of strings
+    for (auto it1 = course_names.begin(); it1 != course_names.end(); ++it1) {
+      for (auto it2 = course_register.begin(); it2 != course_register.end(); ++it2) {
+	if (((*it2)->getName()).compare(*it1) == 0) {
+	  course_prefs.push_front(*it2);
+	}
+      }
+    }
+    
+    // make a new student by passing in name, gpa, course list
+    temp = new Student(student_name, course_prefs, 0, gpa);
+    
+    
+    
+    
+    // cout << "---" << student_name << "---\n";
+    // cout << "---" << gpa << "---\n";
+    // for (auto it = course_prefs.begin(); it != course_prefs.end(); ++it) {
+    //   cout << "---" << (*it)->getName() << "---\n";
+    // }
+    // cout << "\n\n\n";
+
+    // read chars until get to the beginning of next student
+    while (true) {
+      s_file.read(c, 1);
+      if (strcmp(c, "\n") == 0) {
+	break;
+      }
+      else if (strcmp(c, "#") == 0) {
+	goto S_FILE_DONE;
+      }
+    }
+    
+  }
+
+ S_FILE_DONE:
+  s_file.close();
+ 
   // run matching algorithm
-  students = deferred_acceptance(students, courses);
+  deferred_acceptance(all_students, course_register);
   
   // TODO: make a print method to print out the students and courses
   
